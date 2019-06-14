5c2e57b0e2d7cad54c397b676b0104470f590d29
marwicha <marwa.rekik@esprit.tn>
2019-06-14 21:18:25 +0200

    backend:Connect to database mongo and added model expenses

--- server/server.js
+++ server/server.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = require('./routes/routes.js')
 var path = require('path');
 var app = express();
+
+// for database mongo
+var bodyParser = require('body-parser');
+// for database mongo
+var mongoose = require('mongoose');
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client'));
+
+// for connecting to database mongo (https://mlab.com)
 app.use(express.static(path.join(__dirname, '../client')));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
+mongoose.connect('mongodb://admin:admin123@ds235417.mlab.com:35417/expenses');
+
 app.use('/', router);
 module.exports=app;
