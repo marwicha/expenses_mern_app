926eb98b4a413608e8ddd6d71c6110545d0eea3a
marwicha <marwa.rekik@esprit.tn>
2019-06-14 21:54:43 +0200

    backend: added method getAll expenses

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -9,5 +9,23 @@ router.get('/', function(req, res){
 });
 
 
+router.get('/getAll',function(req, res) {
+  var monthRec = req.query.month;
+  var yearRec = req.query.year;
+  if(monthRec && monthRec !== 'All'){
+   Expense.find({$and: [ {month: monthRec}, {year: yearRec}]}, function(err, expenses) {
+    if (err)
+     res.send(err);
+    res.json(expenses);
+   });
+  } else {
+   Expense.find({year: yearRec}, function(err, expenses) {
+    if (err)
+     res.send(err);
+    res.json(expenses);
+   });
+  }
+ });
+
 
 module.exports = router;
