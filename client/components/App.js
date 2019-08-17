7001f450023c214c0ee84ef831c3434dd53b825c
marwa <marwa.rekik@esprit.tn>
2019-08-17 21:04:33 +0200

    feat(FrontAppjs): minor changes

--- client/components/App.js
+++ client/components/App.js
@@ -1,54 +1,58 @@
 import React from 'react';
 import axios from 'axios';
+import Add from './Add'
 
 export default class App extends React.Component {
 
   constructor() {
     super();
-    this.state = {selectedMonth:'Jan', selectedYear: '2019', allExpenses: []};
-    this.getAllExpenses = this.getAllExpenses.bind(this);
+    this.state = {selectedMonth:'All', selectedYear: 2019, data: []};
+    this.getData = this.getData.bind(this);
   }
 
-  componentDidMount() {
-    this.getAllExpenses(this, '2019');
-  }
-  componentWillReceiveProps(nextProps) {
-    this.getAllExpenses(this, '2019');
+componentDidMount() {
+    this.getData(this, '2019');
   }
 
-  getAllExpenses(ev, year) {
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    this.getData(this, '2019');
+  }
+
+getData(ev, year){
     axios.get('/getAll?month=All&year='+year)
-      .then(function(response) {
-        ev.setState({allExpenses: response.data});
+      .then((response)=>  {
+        ev.setState({data: response.data});
         ev.setState({selectedYear: parseInt(year)})
       });
   }
  
- render() {
+  render() {
     return (
       <div>
+        <Add selectedMonth={this.state.selectedMonth} 
+             selectedYear={this.state.selectedYear} />
         <table>
           <thead>
             <tr><th></th><th className='desc-col'>Description</th>
             <th className='button-col'>Amount</th>
             <th className='button-col'>Month</th>
             <th className='button-col'>Year</th>
-            </tr>
+           </tr>
           </thead>
           <tbody>
             {
-              this.state.allExpenses.map((exp) => {
-                return  <tr>
-                  <td className='counterCell'></td>
-                  <td className='desc-col'>{exp.description}</td>
+              this.state.data.map(function(exp){
+                return  <tr><td className='counterCell'></td>
+                <td className='desc-col'>{exp.description}</td>
                 <td className='button-col'>{exp.amount}</td>
                 <td className='button-col'>{exp.month}</td>
                 <td className='button-col'>{exp.year}</td>
+                
                 </tr>
               })
             }
-           </tbody>
-         </table>
+            </tbody>
+          </table>
       </div>
     );
   }
